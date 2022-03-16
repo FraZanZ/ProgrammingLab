f80766b68a3ee233de8f4d0652d9605a2a4ec033
Francesca <90377236+FraZanZ@users.noreply.github.com>
2022-03-16 11:29:36 +0000

    con intestazione

--- esercizio1_lezione2.py
+++ esercizio1_lezione2.py
@@ -1,18 +1,32 @@
-# Inizializzo una lista vuota per salvare i valori 
-values = [] # Apro e leggo il file, linea per linea 
-my_file = open('shampoo_sales.csv', 'r') 
-for line in my_file:   
-    # Faccio lo split di ogni riga sulla virgola   
-    elements = line.split(',')   # Se NON sto processando l’intestazione...    
-if elements[0] != 'Date':        # Setto la data e il valore        
-   date  = elements[0]        
-   value = elements[1]        
-# Aggiungo alla lista dei valori questo valore         
-   #values.append(value)
-# trasformo in float i valori in values
-values_float = list(map(float, values))
-print(values[1:5])
+def sum_list(the_list):
+    if not the_list:
+        return None
+    else:
+        total = 0
+        for element in the_list:
+            total += element
+        return total
 
-print(values_float[1:5])
-somma = sum(values_float)
-print(somma)
+
+#file_name = 'shampoo_sales.csv'
+def sum_csv(file_name):
+    if not file_name:
+        return None
+    else:
+
+# Inizializzo una lista vuota per salvare i valori 
+         values = [] # Apro e leggo il file, linea per linea 
+
+    my_file = open(file_name, 'r') 
+    for line in my_file:   
+    # Faccio lo split di ogni riga sulla virgola   
+        elements = line.split(',')   # Se NON sto processando l’intestazione...    
+        if elements[0] != 'Date':        # Setto la data e il valore        
+           date  = elements[0]        
+           value = elements[1]        
+# Aggiungo alla lista dei valori questo valore         
+           values.append(value)
+        my_file.close()
+        return sum_list(values)
+#Provo
+print(sum_csv('shampoo_sales.csv'))
