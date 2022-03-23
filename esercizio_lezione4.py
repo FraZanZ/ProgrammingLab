fa4a30b395acc51c38254a4773606dce77d4d19f
Francesca <90377236+FraZanZ@users.noreply.github.com>
2022-03-23 10:24:09 +0000

    sistemato print e for

--- esercizio_lezione4.py
+++ esercizio_lezione4.py
@@ -4,31 +4,29 @@ class CSVFile():
     def __init__(self, name):
     #Set name
         self.name = name
-
-  #  def __str__(self):
-  #      return 'CSVFile "{}"'.format(self.name)
-
-#nomefile = CSVFile('shampoo_sales.csv')
-#print(nomefile)
-# inizializzo con il nome del file csv
     
-# creo un attibuto name che contenga il nome del file csv
-
 # creo metodo  “get_data()” che torni i dati dal file CSV come lista di liste,  [ ['01-01-2012', '266.0'], ['01-02-2012', '145.9'], ... ]
     def get_data(self):
-        list = []
         my_file = open(self.name,'r')
+        #inizializzo la lista
+        list = []
+        #ciclo sulle righe del file
         for line in my_file:   
             # Faccio lo split di ogni riga sulla virgola   
-                elements = line.split(',')   # Se NON sto processando l’intestazione... 
-                elements[-1] = elements[-1].strip() # con -1 accedo all'ultimo carattere e tolgo dall'ultimo elemento il newline \n strip toglie le porcherie
-                if elements[0] != 'Date':        # Setto la data e il valore        
-                   date  = elements[0]        
-                   value = elements[1]        
+            line = line.strip()   
+            #
+            line_elements = line.split(',')   # Se NON sto processando l’intestazione... 
+                #elements[-1] = elements[-1].strip() # con -1 accedo all'ultimo carattere e tolgo dall'ultimo elemento il newline \n strip toglie le porcherie
+            if not line_elements[0].startswith('Date'):        # Setto la data e il valore             
         # Aggiungo alla lista dei valori questo valore         
-                   line.append(elements) #mettebndo elements invece che [date,value] potrei avere più colonne
+                   list.append(line_elements) #mettebndo elements invece che [date,value] potrei avere più colonne
         return list
 
-#csvfile = CSVFile('shampoo_sales.csv')
-#print(csvfile.get_data())
- 
+# Controllo se funziona (su bash)
+       
+#==============================
+#  Main del programma
+#==============================
+# srtampo a video
+# csv_file = CSVFile('shampoo_sales.csv')
+# print(csv_file.get_data())
