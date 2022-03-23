512751d34dd6a743b905aa48360d162dce99118a
Francesca <90377236+FraZanZ@users.noreply.github.com>
2022-03-23 09:03:31 +0000

    aggiunto funzione eleva_al_quadrato

--- esercizi_prova_lezione2.py
+++ esercizi_prova_lezione2.py
@@ -3,3 +3,14 @@ def mia_funzione(argomento1, argomento2):
 
 
 mia_funzione("Pippo","Pluto")
+
+
+
+def eleva_al_quadrato(numero):
+    return numero*numero
+
+
+number = 7
+print("number: {} ".format(number))
+risultato = eleva_al_quadrato(number)
+print("Risultato: {} ".format(risultato))
